24792a8f23ad9bc9df502d839660db49a3331ada
Sanjay Kalluparambil <sanjay.kalluparambil@gmail.com>
2018-10-10 14:48:35 -0400

    Create RunDBImports.bat

---
+++ bcp/RunDBImports.bat
@@ -0,0 +1,11 @@
+@echo off
+:: Wrapper procedure to run each batch file sequencially
+::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo ------------------------------------------------
+echo 			 Starting import-cat-items
+echo ------------------------------------------------
+call import-cat-items.bat
+::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::
+::
