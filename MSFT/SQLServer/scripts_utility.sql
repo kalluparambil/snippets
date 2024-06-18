6c904c34d4e997caebe0d9adc8327cede949e2c2
Sanjay Kalluparambil <sanjay.kalluparambil@gmail.com>
2024-06-18 17:04:00 -0400

    Create scripts_utility.sql

---
+++ MSFT/SQLServer/scripts_utility.sql
@@ -0,0 +1,17 @@
+--Scripts to drop tables in a schema
+```
+select -- top 100 *
+ table_name
+,'drop table if exists ' 
++ table_schema 
++ '.' 
++ table_name 
++ ' ;' 
+as stmt
+from information_schema.tables
+where 1=1
+--and table_catalog = 'DATABASE_NAME'
+and table_schema = 'DBO'
+and upper(table_name) like '%SK_TEMP%'
+;
+```
